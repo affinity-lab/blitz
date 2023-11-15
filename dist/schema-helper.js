4b1c8dcec8df60496341c63b2061222d3b969912
Gergely Laborci <gergely@laborci.hu>
2023-11-15 22:08:37 +0100

    schema-helper / reference field arg must be a function

--- dist/schema-helper.js
+++ dist/schema-helper.js
@@ -4,6 +4,8 @@ exports.reference = exports.id = void 0;
 const mysql_core_1 = require("drizzle-orm/mysql-core");
 const id = () => (0, mysql_core_1.int)("id").autoincrement().primaryKey();
 exports.id = id;
-const reference = (name, field, nullable = false) => nullable ? (0, mysql_core_1.int)(name).references(() => field) : (0, mysql_core_1.int)(name).notNull().references(() => field);
+const reference = (name, field, nullable = false) => nullable
+    ? (0, mysql_core_1.int)(name).references(field)
+    : (0, mysql_core_1.int)(name).notNull().references(field);
 exports.reference = reference;
 //# sourceMappingURL=schema-helper.js.map
